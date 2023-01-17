9fb51a8c62b2d5420f28857c1be26189b58972de
Wesley <wesley.ferreira@santacasaitajuba.com.br>
2023-01-17 13:07:20 -0300

    vinculando tabelas

--- nova_view_epimed.sql
+++ nova_view_epimed.sql
@@ -2,3 +2,15 @@
 -- Date: 17 de jan. de 2023
 -- Time: 12:54:16
 -- By Analista Wesley
+
+
+
+SELECT 
+
+*
+
+
+FROM ATCABECATEND ATCABECATEND  /*Atendimento -Cabecalho de atendimento*/
+JOIN RICADPAC RICADPAC ON ATCABECATEND.ID_RICADPAC = RICADPAC.ID /*Cadastro de Paciente*/ 
+JOIN RICADINT RICADINT ON RICADINT.ID_ATCABECATEND = ATCABECATEND.ID /*Recepção Interna - Cadastro de internações*/ 
+JOIN RILEITOS RILEITOS ON RICADINT.ID_RILEITOS = RILEITOS.ID /*Recepção Interna - Cadastro de leitos*/ 
