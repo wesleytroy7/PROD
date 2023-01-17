b04cfe4202efd332dac1d110893f76a4c4fdd33c
Wesley <wesley.ferreira@santacasaitajuba.com.br>
2023-01-17 16:57:09 -0300

    verificando as diferenças entre dados rileitos e rictrloc, criado tabela derivada para pegar e comparar bloco, leito, acomodação de rictrloc com a rileitos para pegar id dos leitos

--- nova_view_epimed.sql
+++ nova_view_epimed.sql
@@ -3,14 +3,68 @@
 -- Time: 12:54:16
 -- By Analista Wesley
 
+/*
+ * view epimed
+ * prontuario
+ * cpf
+ * rg
+ * nome paciente
+ * sexo 
+ * data de nascimento
+ * convenio
+ * hospital code CAST(NULL AS VARCHAR(10)) AS HOSPITALCODE,
+ * registro 
+ * data/hora entrada
+ * bloco
+ * cod atendimento
+ * data/hora na unidade
+ * id do leito
+ * RICADINT.MOTIVO  AS DISCHARGECAUSE,
+ *  linha 37 a 65 da view V_INTEGRACAO_SCMI_X_EPIMED
+ * 
+ * 
+ * 
+ * */
 
 
 SELECT 
 
 *
 
+FROM (
+SELECT 
+--DISTINCT 
+RICADPAC.PRONT AS MEDICALRECORD,
+RICADPAC.CPF,
+RICADPAC.IDENT,
+RICADPAC.NOME,
+--RILEITOS.ANDAR,
+
+
+
+RICTRLOC.BLOCO AS blocoLOC, 
+--RILEITOS.BLOCO,
+RICTRLOC.LEITO AS leitoLOC2,
+--RILEITOS.LEITO,
+RICTRLOC.ACOMOD acomodLOC2,
+--RILEITOS.ACOMOD, 
+
+RICADINT.ENTRADA
 
 FROM ATCABECATEND ATCABECATEND  /*Atendimento -Cabecalho de atendimento*/
 JOIN RICADPAC RICADPAC ON ATCABECATEND.ID_RICADPAC = RICADPAC.ID /*Cadastro de Paciente*/ 
 JOIN RICADINT RICADINT ON RICADINT.ID_ATCABECATEND = ATCABECATEND.ID /*Recepção Interna - Cadastro de internações*/ 
-JOIN RILEITOS RILEITOS ON RICADINT.ID_RILEITOS = RILEITOS.ID /*Recepção Interna - Cadastro de leitos*/ 
+--JOIN RILEITOS RILEITOS ON RICADINT.ID_RILEITOS = RILEITOS.ID /*Recepção Interna - Cadastro de leitos*/  
+JOIN RICTRLOC RICTRLOC ON RICTRLOC.REG = RICADINT.REG  /*Recepção Interna - Controle de localizações*/
+where ((atcabecatend.data_hora_alta_medica >= current_timestamp - 20) or (atcabecatend.data_hora_alta_medica is null))
+      and atcabecatend.tp_atendimento = 'I'
+ 
+
+ORDER BY RICADPAC.NOME asc
+
+)EPIMED
+
+
+
+
+
