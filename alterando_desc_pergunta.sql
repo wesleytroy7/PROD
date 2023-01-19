a3eba697faeb4a51494d0c47bc475570aef625b4
Wesley <wesley.ferreira@santacasaitajuba.com.br>
2023-01-19 07:39:10 -0300

    script para alteração de descrição de pergunta quando escrito errado

---
+++ alterando_desc_pergunta.sql
@@ -0,0 +1,15 @@
+-- New script in PROD.
+-- Date: 12 de jan. de 2023
+-- Time: 08:39:11
+-- By Analista Wesley
+
+
+
+--select P.DESCRICAO  from PRPERGUNTA p WHERE P.ID =3680
+--;
+--
+--
+--
+--UPDATE PRPERGUNTA
+--	SET DESCRICAO='->'
+--	WHERE ID=3692;
