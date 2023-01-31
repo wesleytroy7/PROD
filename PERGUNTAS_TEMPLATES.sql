55e5407ca7203040f9ed7c1c88cbc9084e709074
Informatica <informatica@SCM.ITAJUBA>
2023-01-31 16:57:45 -0300

    perguntas e respostas templates

---
+++ PERGUNTAS_TEMPLATES.sql
@@ -0,0 +1,20 @@
+-- CONNECTION: name=PROD
+-- New script in PROD.
+-- Date: 31 de jan. de 2023
+-- Time: 16:22:55
+-- By Analista Wesley
+/*
+ *    
+ * 
+ */
+
+
+
+SELECT 
+
+PREVOLUCAO.* 
+FROM PREVOLUCAO PREVOLUCAO 
+WHERE 
+PREVOLUCAO.COD IS NOT DISTINCT :pCOD
+
+
